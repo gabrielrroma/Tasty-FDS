551db5c00e55dcccc1d4c8ad7dd58deb8a2e98f6
Gabriel Roma <gabrielrroma@gmail.com>
2023-10-26 19:33:13 -0300

    deployment

--- Tasty/settings.py
+++ Tasty/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%(_n&5+-6&1ruj_a-qtkj9+87&_u8q-6k4r23o7ksrie(*w)5l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['tastyflavorfusion.azurewebsites.net']
 
 
 # Application definition
